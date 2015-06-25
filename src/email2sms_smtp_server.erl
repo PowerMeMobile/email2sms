7e240ae22db87e465fa5f0ac1dafc3fdf8a1bc14
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-06-25 15:50:27 +0300

    Implement supported fields and local domain filtering

--- src/email2sms_smtp_server.erl
+++ src/email2sms_smtp_server.erl
@@ -26,7 +26,22 @@
 -include("application.hrl").
 -include_lib("alley_common/include/logging.hrl").
 
--record(st, {}).
+%-define(TEST, 1).
+-ifdef(TEST).
+    -include_lib("eunit/include/eunit.hrl").
+    -compile(export_all).
+-endif.
+
+-type email() :: binary().
+
+-record(st, {
+    type :: binary(),
+    subtype :: binary(),
+    headers :: [{binary(), binary()}],
+    params  :: [{binary(), term()}],
+    content :: term(),
+    recipients :: [email()]
+}).
 
 %% ===================================================================
 %% API
@@ -103,16 +118,40 @@ handle_RCPT_extension(Extension, _St) ->
     error.
 
 handle_DATA(From, To, Data, St) ->
-    ?log_debug("Got an email (from: ~s, to: ~s)",
-        [From, string:join([binary_to_list(A) || A <- To], ", ")]),
-    try
-        do_handle_DATA(From, To, Data, St)
-    catch
-        Exc:Cls ->
-            ?log_error("Exception: ~p:~p", [Exc, Cls]),
-            ?log_error("Probably too deep mime nesting met", []),
-            {error, "554 MIME type not supported.", St}
-    end.
+    ?log_debug("Got an email (from: ~p, to: ~p)", [From, To]),
+
+    {Type, Subtype, Headers, Params, Content} = mimemail:decode(Data),
+
+    %% lower case keys.
+    Headers2 = [{binstr:to_lower(K), V} || {K, V} <- Headers],
+
+    %% use already cleaned up from.
+    Headers3 = lists:keyreplace(<<"from">>, 1, Headers2, {<<"from">>, From}),
+
+    %% cleanup to, cc and recover bcc.
+    {To2, Cc, Bcc} = recover_to_cc_bcc(To, Headers3),
+    Headers4 = lists:keyreplace(<<"to">>, 1, Headers3, {<<"to">>, To2}),
+    Headers5 = lists:keyreplace(<<"cc">>, 1, Headers4, {<<"cc">>, Cc}),
+    Headers6 = [{<<"bcc">>, Bcc} | Headers5],
+
+    ?log_debug("~p", [Type]),
+    ?log_debug("~p", [Subtype]),
+    ?log_debug("~p", [Headers2]),
+    ?log_debug("~p", [Headers6]),
+    %?log_debug("~p", [Params]),
+    %?log_debug("~p", [Content]),
+
+    %ContentType = <<Type/binary, "/", Subtype/binary>>,
+
+    St2 = St#st{
+        type = Type,
+        subtype = Subtype,
+        headers = Headers6,
+        params = Params,
+        content = Content
+    },
+
+    handle_data(filter_recipients, St2).
 
 handle_RSET(_St) ->
     #st{}.
@@ -138,8 +177,115 @@ terminate(Reason, St) ->
 %% Internal
 %% ===================================================================
 
-do_handle_DATA(From, To, Data, St) ->
-    {_Type, _Subtype, Headers, _Params, _Content} =
-        Component = mimemail:decode(Data),
+handle_data(filter_recipients, St) ->
+    {ok, Fields} = application:get_env(?APP, smtp_recipient_fields),
+    {ok, Domains} = application:get_env(?APP, smtp_local_domains),
+    {ok, MaxRecipients} = application:get_env(?APP, smtp_max_recipient_count),
+    Recipients = collect_recipients_from_fields(St#st.headers, Fields),
+    Recipients2 = filter_recipients_using_domains(Recipients, Domains),
+    Count = length(Recipients2),
+    if
+        Count =:= 0 ->
+            {error, "550 No valid recipients found", St};
+        Count > MaxRecipients ->
+            {error, "550 Too many recipients specified", St};
+        true ->
+            handle_data(authenticate_subject, St#st{recipients = Recipients2})
+    end;
+handle_data(authenticate_subject, St) ->
+    {error, "502 Not implemented", St};
+handle_data(prepare_body, St) ->
+    {error, "502 Not implemented", St};
+handle_data(send, St) ->
+
     UUID = uuid:unparse(uuid:generate()),
-    {ok, UUID, St}.
+    {ok, UUID, St},
+    {error, "502 Not implemented", St}.
+
+recover_to_cc_bcc(All, Headers) ->
+    To = parse_addresses(proplists:get_value(<<"to">>, Headers, [])),
+    Cc = parse_addresses(proplists:get_value(<<"cc">>, Headers, [])),
+    Bcc = (All -- To) -- Cc,
+    {To, Cc, Bcc}.
+
+%% get rid of possible names in form `"name" <address@host.domain>'
+%% variations. only return `address@host.domain' parts
+parse_addresses(AddrsRaw) ->
+    {ok, AddrsParsed} = smtp_util:parse_rfc822_addresses(AddrsRaw),
+    [list_to_binary(Addr) || {_Name, Addr} <- AddrsParsed].
+
+collect_recipients_from_fields(Headers, Fields) ->
+    collect_recipients_from_fields(Headers, Fields, []).
+
+collect_recipients_from_fields(_Headers, [], Acc) ->
+    Acc;
+collect_recipients_from_fields(Headers, [F|Fs], Acc) ->
+    Acc2 = Acc ++ proplists:get_value(F, Headers, []),
+    collect_recipients_from_fields(Headers, Fs, Acc2).
+
+filter_recipients_using_domains(Recipients, Domains) ->
+    filter_recipients_using_domains(Recipients, Domains, []).
+
+filter_recipients_using_domains([], _Domains, Acc) ->
+    lists:reverse(Acc);
+filter_recipients_using_domains([R|Rs], Domains, Acc) ->
+    [_Addr, Domain] = binary:split(R, <<"@">>),
+    case lists:member(Domain, Domains) of
+        true ->
+            filter_recipients_using_domains(Rs, Domains, [R|Acc]);
+        false ->
+            filter_recipients_using_domains(Rs, Domains, Acc)
+    end.
+
+%% ===================================================================
+%% Begin Tests
+%% ===================================================================
+
+-ifdef(TEST).
+
+parse_addresses_test() ->
+    ?assertEqual([<<"to@m.c">>],
+        parse_addresses(<<"\"to\" <to@m.c>">>)),
+    ?assertEqual([<<"to@m.c">>],
+        parse_addresses(<<"\"to@m.c\" <to@m.c>">>)),
+    ?assertEqual([<<"to@m.c">>],
+        parse_addresses(<<"<to@m.c>">>)),
+    ?assertEqual([<<"to@m.c">>],
+        parse_addresses(<<"to@m.c">>)),
+    ?assertEqual([<<"to@m.c">>, <<"to2@m.c">>, <<"to3@m.c">>, <<"to4@m.c">>],
+        parse_addresses(
+            <<"\"to\" <to@m.c>,\"to2@m.c\" <to2@m.c>,<to3@m.c>,to4@m.c">>)).
+
+recover_to_cc_bcc_test() ->
+    ?assertEqual({[], [], []}, recover_to_cc_bcc([], [])),
+    ?assertEqual({[<<"to@m.c">>], [<<"cc@m.c">>], [<<"bcc@m.c">>]},
+        recover_to_cc_bcc(
+            [<<"to@m.c">>, <<"cc@m.c">>, <<"bcc@m.c">>],
+            [{<<"to">>,<<"to@m.c">>},
+             {<<"cc">>,<<"cc@m.c">>}])).
+
+collect_recipients_from_fields_test() ->
+    Headers = [{<<"to">>,[<<"to@m.c">>]},
+               {<<"cc">>,[<<"cc@m.c">>]},
+               {<<"bcc">>,[<<"bcc@m.c">>]}],
+    Fields0 = [],
+    FieldsCc = [<<"cc">>],
+    FieldsAll = [<<"to">>, <<"cc">>, <<"bcc">>],
+    ?assertEqual([], collect_recipients_from_fields(Headers, Fields0)),
+    ?assertEqual([<<"cc@m.c">>], collect_recipients_from_fields(Headers, FieldsCc)),
+    ?assertEqual([<<"to@m.c">>, <<"cc@m.c">>, <<"bcc@m.c">>], collect_recipients_from_fields(Headers, FieldsAll)).
+
+filter_recipients_using_domains_test() ->
+    Recipients = [<<"a@m.c">>, <<"b@n.c">>, <<"c@m.d">>, <<"d@n.d">>],
+    Domains0 = [],
+    Domains1 = [<<"m.c">>],
+    Domains2 = [<<"n.c">>, <<"n.d">>, <<"n.e">>],
+    ?assertEqual([], filter_recipients_using_domains(Recipients, Domains0)),
+    ?assertEqual([<<"a@m.c">>], filter_recipients_using_domains(Recipients, Domains1)),
+    ?assertEqual([<<"b@n.c">>, <<"d@n.d">>], filter_recipients_using_domains(Recipients, Domains2)).
+
+-endif.
+
+%% ===================================================================
+%% End Tests
+%% ===================================================================
