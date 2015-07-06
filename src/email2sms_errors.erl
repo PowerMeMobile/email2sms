7c3dcff70f079df01b05384273c0786d749caa28
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-07-06 17:53:59 +0300

    Refine errors

--- src/email2sms_errors.erl
+++ src/email2sms_errors.erl
@@ -13,7 +13,11 @@
 -spec format_error(atom() | binary() | list()) -> list().
 format_error(invalid_credentials) ->
     ?E_AUTHENTICATION;
+format_error(originator_not_found) ->
+    ?E_NO_ORIGINATOR;
 format_error(no_recipients) ->
+    ?E_NO_RECIPIENTS;
+format_error(no_dest_addrs) ->
     ?E_NO_RECIPIENTS;
 format_error(timeout) ->
     ?E_TIMEOUT;
@@ -21,9 +25,13 @@ format_error(server_busy) ->
     ?E_SERVER_BUSY;
 format_error(send_failed) ->
     ?E_SEND_FAILED;
+format_error(invalid_recipient_policy) ->
+    ?E_INVALID_RECIPIENT_POLICY;
+format_error(credit_limit_exceeded) ->
+    ?E_CREDIT_LIMIT_EXCEEDED;
 format_error(Error) when is_atom(Error) ->
-    atom_to_list(Error);
+    ["550 ", atom_to_list(Error)];
 format_error(Error) when is_binary(Error) ->
-    binary_to_list(Error);
+    ["550 ", binary_to_list(Error)];
 format_error(Error) when is_list(Error) ->
-    Error.
+    ["550 ", Error].
--- src/email2sms_errors.hrl
+++ src/email2sms_errors.hrl
@@ -1,16 +1,19 @@
 -ifndef(email2sms_errors_hrl).
 -define(email2sms_errors_hrl, defined).
 
--define(E_AUTHENTICATION,       "550 Invalid user account").
--define(E_INTERNAL,             "554 Internal server error").
--define(E_NOT_IMPLEMENTED,      "502 Not implemented").
--define(E_TIMEOUT,              "554 Request timeout").
--define(E_NO_RECIPIENTS,        "550 No valid recipients found").
--define(E_TOO_MANY_RECIPIENTS,  "550 Too many recipients specified").
--define(E_SERVER_BUSY,          "554 Server is busy").
--define(E_NOT_RECOGNIZED,       "500 Not recognized").
--define(E_TOO_MANY_PARTS,       "550 Too many SMS parts").
--define(E_UNKNOWN_CONTENT_TYPE, "550 Unknown content type").
--define(E_SEND_FAILED,          "550 Send failed").
+-define(E_AUTHENTICATION,           "550 Invalid user account").
+-define(E_INTERNAL,                 "554 Internal server error").
+-define(E_NOT_IMPLEMENTED,          "502 Not implemented").
+-define(E_TIMEOUT,                  "554 Request timeout").
+-define(E_NO_ORIGINATOR,            "550 Originator is not found").
+-define(E_NO_RECIPIENTS,            "550 No valid recipients found").
+-define(E_TOO_MANY_RECIPIENTS,      "550 Too many recipients specified").
+-define(E_SERVER_BUSY,              "554 Server is busy").
+-define(E_NOT_RECOGNIZED,           "500 Not recognized").
+-define(E_TOO_MANY_PARTS,           "550 Too many SMS parts").
+-define(E_UNKNOWN_CONTENT_TYPE,     "550 Unknown content type").
+-define(E_SEND_FAILED,              "550 Send failed").
+-define(E_INVALID_RECIPIENT_POLICY, "550 Rejected by invalid recipient policy").
+-define(E_CREDIT_LIMIT_EXCEEDED,    "550 Customer's credit limit is exceeded").
 
 -endif.
